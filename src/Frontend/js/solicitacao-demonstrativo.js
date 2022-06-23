de777e4da0d32d42866d439661ebd8899e7f0c43
cazemoises <99195695+cazemoises@users.noreply.github.com>
2022-06-23 01:39:10 -0300

    q

--- src/Frontend/js/solicitacao-demonstrativo.js
+++ src/Frontend/js/solicitacao-demonstrativo.js
@@ -1,9 +1,9 @@
-var currentVal = sessionStorage.getItem("currentVal");
-var logged_cnpj = sessionStorage.getItem("logged_cnpj");
-var regra = sessionStorage.getItem("regra");
-var valorEscolhido = sessionStorage.getItem("valorEscolhido");
-var resultado = sessionStorage.getItem("resultado");
-var d;
+var currentVal = sessionStorage.getItem("currentVal")
+var logged_cnpj = sessionStorage.getItem("logged_cnpj")
+var regra = sessionStorage.getItem("regra")
+var valorEscolhido = sessionStorage.getItem("valorEscolhido")
+var resultado = sessionStorage.getItem("resultado")
+var d
 
 function changePage(){
     window.location = '../html/Senha.html';
@@ -12,16 +12,16 @@ function teste() {
   console.log(currentVal + " " + logged_cnpj + " " + regra + " " + valorEscolhido + " " + resultado)
 }
 function viewAnticipation() {
-        if (regra == 2) {
+        if (regra == "2") {
           d = '2'
         }
-        else if (regra == 7) {
+        else if (regra == "7") {
           d = '7'
         }
-        else if (regra == 15) {
+        else if (regra == "15") {
           d = '15'
         }
-        else if (regra == 30) {
+        else if (regra == "30") {
           d = '30'
         }
         var data = new Date();
@@ -55,7 +55,7 @@ function viewAnticipation() {
               dianome = "Sábado";
         }
         $("#datenow").html(`${dia}/${mes}/${ano} - ${dianome}`);
-        $("#ds").html(`D+${d} (${regra}%)`);
+        $("#ds").html(`D+${} (${regra}%)`);
         $("#choosen-value").html(`R$ ${valorEscolhido}`);
         $("#final-value").html(`R$ ${resultado}`)
 }
