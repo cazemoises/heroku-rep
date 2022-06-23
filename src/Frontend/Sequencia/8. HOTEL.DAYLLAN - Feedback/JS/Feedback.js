275b0121ff5c619b52407719276425dbb75a959d
cazemoises <99195695+cazemoises@users.noreply.github.com>
2022-06-22 21:19:11 -0300

    f

--- src/Frontend/Sequencia/8. HOTEL.DAYLLAN - Feedback/JS/Feedback.js
+++
@@ -1,4 +0,0 @@
-
-function changePage(){
-    window.location='file:///C:/Users/Inteli/Documents/GitHub/2022.M2.T4-PROJETO4/src/Frontend/Sequencia/1.%20HOTEL.JORDAN%20-%20Conta/HTML/mainHoteleiro.html'
-}
